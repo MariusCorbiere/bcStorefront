bc91941ba4be1c581bda2fe00f6e7a21cd4ada70
Mars Ender <marsender@free.fr>
2022-08-24 13:10:40 +0200

    Initial work

--- src/app/core/components/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/core/components/terms-and-conditions/terms-and-conditions.component.ts
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component, OnInit, LOCALE_ID, Inject } from '@
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TermsAndConditionsComponent implements OnInit {
+
   constructor(@Inject(LOCALE_ID) public locale: string) { }
 
   ngOnInit(): void {
