fc4631b10ab25cd6106fb05e3da3b89adffb1352
Mars Ender <marsender@free.fr>
2022-08-28 22:45:10 +0200

    feat : add page-not-found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ProductDetailComponent } from './core/components/product-detail/product
 import { ProductListComponent } from './core/components/product-list/product-list.component';
 import { TermsAndConditionsComponent } from './core/components/terms-and-conditions/terms-and-conditions.component';
 import { StorytellingComponent } from './core/components/storytelling/storytelling.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 export const routes: Route[] = [
     {
@@ -20,19 +21,23 @@ export const routes: Route[] = [
         component: ProductDetailComponent,
     },
     {
-        path: 'terms-and-conditions',
-        component: TermsAndConditionsComponent,
-    },
-    {
-        path: 'storytelling',
-        component: StorytellingComponent,
-    },
-    {
         path: 'account',
         loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
     },
     {
         path: 'checkout',
         loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule),
+    },
+    {
+      path: 'terms-and-conditions',
+      component: TermsAndConditionsComponent,
+    },
+    {
+      path: 'storytelling',
+      component: StorytellingComponent,
+    },
+    {
+      path: '**',
+      component: PageNotFoundComponent,
     },
 ];
